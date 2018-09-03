f3fdaaf095607a04007eb4faefee76674cce7148
sature <dev.sickshell@gmail.com>
2018-09-03 17:22:39 +0800

    initial version

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,9 +5,9 @@ sys.path.append(os.path.split(os.path.realpath(__file__))[0])
 
 from Application import Application as App
 from K import K
-from Shout import Shout
 from Contract import Contract
 from Observable import Observable
 
-__all__ = ['App', 'K', 'Contract', 'Observable', 'Shout']
+
+__all__ = ['App', 'K', 'Contract', 'Observable']
 
